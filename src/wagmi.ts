6895148a2b7b592c96b1276435df999c82d06735
EliteGoodDev <michaeljohnares.1026@gmail.com>
2025-07-17 13:30:20 -0500

    change wagmi config

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,11 +1,10 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { mainnet, sepolia } from 'wagmi/chains';
+import { sepolia } from 'wagmi/chains';
 
 export const config = getDefaultConfig({
   appName: 'RainbowKit App',
   projectId: 'YOUR_PROJECT_ID',
   chains: [
-    mainnet,
     sepolia,
   ],
   ssr: true,
